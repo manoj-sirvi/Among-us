704f0bbdd6180236f32b261f09d177ea1bb17bfb
manojsirvi174 <manojsirvi174@gmail.com>
2021-03-26 01:49:36 +0530

    did

---
+++ build/CMakeFiles/Hello-World.dir/DependInfo.cmake
@@ -0,0 +1,38 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/manoj/Documents/sem4/computer_graphics/Assignment2/Among-us/source/ball.cpp" "/home/manoj/Documents/sem4/computer_graphics/Assignment2/Among-us/build/CMakeFiles/Hello-World.dir/source/ball.cpp.o"
+  "/home/manoj/Documents/sem4/computer_graphics/Assignment2/Among-us/source/color.cpp" "/home/manoj/Documents/sem4/computer_graphics/Assignment2/Among-us/build/CMakeFiles/Hello-World.dir/source/color.cpp.o"
+  "/home/manoj/Documents/sem4/computer_graphics/Assignment2/Among-us/source/input.cpp" "/home/manoj/Documents/sem4/computer_graphics/Assignment2/Among-us/build/CMakeFiles/Hello-World.dir/source/input.cpp.o"
+  "/home/manoj/Documents/sem4/computer_graphics/Assignment2/Among-us/source/main.cpp" "/home/manoj/Documents/sem4/computer_graphics/Assignment2/Among-us/build/CMakeFiles/Hello-World.dir/source/main.cpp.o"
+  "/home/manoj/Documents/sem4/computer_graphics/Assignment2/Among-us/source/nonedit.cpp" "/home/manoj/Documents/sem4/computer_graphics/Assignment2/Among-us/build/CMakeFiles/Hello-World.dir/source/nonedit.cpp.o"
+  "/home/manoj/Documents/sem4/computer_graphics/Assignment2/Among-us/source/other_handlers.cpp" "/home/manoj/Documents/sem4/computer_graphics/Assignment2/Among-us/build/CMakeFiles/Hello-World.dir/source/other_handlers.cpp.o"
+  "/home/manoj/Documents/sem4/computer_graphics/Assignment2/Among-us/source/timer.cpp" "/home/manoj/Documents/sem4/computer_graphics/Assignment2/Among-us/build/CMakeFiles/Hello-World.dir/source/timer.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "GLFW_INCLUDE_NONE"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../source"
+  "../libraries/glfw/include"
+  "../libraries/glad/include"
+  "../libraries/glm"
+  "/usr/include/libdrm"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/manoj/Documents/sem4/computer_graphics/Assignment2/Among-us/build/libraries/glfw/src/CMakeFiles/glfw.dir/DependInfo.cmake"
+  "/home/manoj/Documents/sem4/computer_graphics/Assignment2/Among-us/build/CMakeFiles/glad.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
